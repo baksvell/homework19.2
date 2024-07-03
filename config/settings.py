2776f910a4fa964de3929f35fbd0dbd0cb6adaad
Baksvell <Patsi2@yandex.ru>
2024-07-03 13:34:28 +0400

    add homework 20.2

--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'Product',
         'USER': 'postgres',
-        'PASSWORD': '53_Gagauz',
+        'PASSWORD': 'brnlol479',
 
     }
 }
