c95e721ec448c4dff011539c79bc02d6a6e468c7
Baksvell <Patsi2@yandex.ru>
2024-07-09 18:20:59 +0400

    add homework 22.1

---
+++ catalog/forms.py
@@ -0,0 +1,58 @@
+from django import forms
+
+from catalog.models import Product, Version
+from common.views import StyleFormMixin
+
+
+class ProductForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ("name", "description", "image", "price", "category")
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data.get("name")
+        words = [
+            "казино",
+            "криптовалюта",
+            "крипта",
+            "биржа",
+            "дешево",
+            "бесплатно",
+            "обман",
+            "полиция",
+            "радар",
+        ]
+        for word in words:
+            if word in cleaned_data:
+                raise forms.ValidationError("Недопустимое значение")
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data.get("description")
+        words = [
+            "казино",
+            "криптовалюта",
+            "крипта",
+            "биржа",
+            "дешево",
+            "бесплатно",
+            "обман",
+            "полиция",
+            "радар",
+        ]
+        for word in words:
+            if word in cleaned_data:
+                raise forms.ValidationError("Недопустимое значение")
+        return cleaned_data
+
+
+class ModeratorProductForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ("category", "description", "is_published")
+
+
+class VersionForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = ("number_version", "name_version", "version_flag")
